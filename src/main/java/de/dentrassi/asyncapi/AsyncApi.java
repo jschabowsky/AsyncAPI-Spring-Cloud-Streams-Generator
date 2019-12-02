a449e545d3db6a71183c8f7cce3530d816bf67af
Jonathan Schabowsky <jonathan.schabowsky@solace.com>
2019-12-02 12:37:09 -0700

    upgraded to 2.0.0

--- src/main/java/de/dentrassi/asyncapi/AsyncApi.java
+++ src/main/java/de/dentrassi/asyncapi/AsyncApi.java
@@ -37,7 +37,7 @@ import de.dentrassi.asyncapi.validate.Validator;
 
 public class AsyncApi {
 
-    public static final String VERSION = "2.0.0-rc1";
+    public static final String VERSION = "2.0.0";
 
     private Info info;
 
